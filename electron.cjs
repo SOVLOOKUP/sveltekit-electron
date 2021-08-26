bca117c2d37e7d7a8c347af71dcf211fe98845aa
sovlookup <gonorth@qq.com>
2021-08-26 22:46:24 +0800

    feat: init

--- electron.cjs
+++ electron.cjs
@@ -1,25 +1,14 @@
-const windowStateManager = require('electron-window-state');
-const contextMenu = require('electron-context-menu');
-const { app, BrowserWindow } = require('electron');
-const serve = require('electron-serve');
+const { app, BrowserWindow } = require('electron')
+const serve = require('electron-serve')
 
-try {
-	require('electron-reloader')(module);
-} catch (e) {
-	console.error(e);
-}
-
-const serveURL = serve({ directory: "." });
-const port = process.env.PORT || 3000;
-const dev = !app.isPackaged;
-let mainWindow;
+const serveURL = serve({ directory: "." })
+const port = process.env.PORT || 3000
+const dev = !app.isPackaged
+let mainWindow
 
 function createWindow() {
-	let windowState = windowStateManager({
-		defaultWidth: 800,
-		defaultHeight: 600,
-	});
-	
+
+
 	const mainWindow = new BrowserWindow({
 		backgroundColor: 'whitesmoke',
 		titleBarStyle: 'hidden',
@@ -37,60 +26,44 @@ function createWindow() {
 			spellcheck: false,
 			devTools: dev,
 		},
-		x: windowState.x,
-		y: windowState.y,
-		width: windowState.width,
-		height: windowState.height,
-	});
-
-	windowState.manage(mainWindow);
+		width: 800,
+		height: 600,
+	})
 
 	mainWindow.once('ready-to-show', () => {
-		mainWindow.show();
-		mainWindow.focus();
-	});
+		mainWindow.show()
+		mainWindow.focus()
+	})
 
 	mainWindow.on('close', () => {
-		windowState.saveState(mainWindow);
-	});
+	})
 
-	return mainWindow;
+	return mainWindow
 }
-
-contextMenu({
-	showLookUpSelection: false,
-	showSearchWithGoogle: false,
-	showCopyImage: false,
-	prepend: (defaultActions, params, browserWindow) => [
-		{
-			label: 'Make App 💻',
-		},
-	],
-});
 
 function loadVite(port) {
 	mainWindow.loadURL(`http://localhost:${port}`).catch((e) => {
-		console.log('Error loading URL, retrying', e);
+		console.log('Error loading URL, retrying', e)
 		setTimeout(() => {
-			loadVite(port);
-		}, 200);
-	});
+			loadVite(port)
+		}, 200)
+	})
 }
 
 function createMainWindow() {
-	mainWindow = createWindow();
-	mainWindow.once('close', () => { mainWindow = null });
+	mainWindow = createWindow()
+	mainWindow.once('close', () => { mainWindow = null })
 
-	if (dev) loadVite(port);
-	else serveURL(mainWindow);
+	if (dev) loadVite(port)
+	else serveURL(mainWindow)
 }
 
-app.once('ready', createMainWindow);
+app.once('ready', createMainWindow)
 app.on('activate', () => {
 	if (!mainWindow) {
-		createMainWindow();
+		createMainWindow()
 	}
-});
+})
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') app.quit();
-});
+	if (process.platform !== 'darwin') app.quit()
+})
