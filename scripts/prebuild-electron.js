2e53d3bc9ef0947abe7799947eee7482fc9c4c15
sovlookup <gonorth@qq.com>
2021-09-16 22:50:50 +0800

    :hammer: use bob-ts to build

---
+++ scripts/prebuild-electron.js
@@ -0,0 +1,18 @@
+import fs from 'fs-extra'
+import dotenv from 'dotenv'
+	; (async () => {
+		dotenv.config()
+		const electronDirName = process.env.ELECTRON_DIR_NAME
+		const outputDir = process.env.OUTPUT_DIR
+
+		// compile electron to cjs
+		if (!fs.existsSync(outputDir)) {
+			await fs.copy(electronDirName, outputDir)
+		}
+
+		if (fs.existsSync('.git')) {
+			await fs.copy('.git', `${outputDir}/.git`)
+		} else {
+			console.warn('Please use git init before release!')
+		}
+	})()
